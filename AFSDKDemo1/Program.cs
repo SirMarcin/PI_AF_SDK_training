6c156474070c1b39395e7f7589fce8e39173d961
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-02-22 12:23:15 +0100

    plot values

--- AFSDKDemo1/Program.cs
+++ AFSDKDemo1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OSIsoft.AF.PI;
+using OSIsoft.AF.Time;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +25,17 @@ namespace AFSDKDemo1
 
             // do smth with the pi system
             // get a pi point 
-            var point = PIPoint.FindPIPoint(piServer, "PLCW00321500=S20NDD11BU900:XQ008");
+            var point = PIPoint.FindPIPoint(piServer, "PLCW00321500=S20NDD11BU900:XQ001");
             var value = point.CurrentValue();
             var timevalue = value.Timestamp;
             var valuevalue = value.Value;
-            
             Console.WriteLine("Tag name: {0} and its Value is: {1}, {2}", point.Name, timevalue, valuevalue);
+
+            var plotValues = point.PlotValues(new AFTimeRange("*-1w", "*"),10);
+            foreach(var pValue in plotValues)
+            {
+                Console.WriteLine("{0} {1}", pValue.Value, pValue.Timestamp);
+            }
             piServer.Disconnect();
 
             Console.ReadKey();
